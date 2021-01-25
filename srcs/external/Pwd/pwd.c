11b65c5ee4eb2c0e521385c3468e041127e5396b
seo2im <seo2im6492@gmail.com>
2021-01-25 21:20:27 +0900

    pwd update

--- srcs/external/Pwd/pwd.c
+++ srcs/external/Pwd/pwd.c
@@ -6,7 +6,7 @@
 /*   By: seolim <seolim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/13 13:49:22 by seolim            #+#    #+#             */
-/*   Updated: 2021/01/13 17:02:41 by seolim           ###   ########.fr       */
+/*   Updated: 2021/01/25 21:12:20 by seolim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,25 @@
 
 int	pwd(int argc, char *argv[], char *envp[])
 {
-	char	*pwd;
+	char	pwd[256];
 
 	argc++;
 	argv++;
+	envp++;
+	if (!getcwd(pwd, 256))
+	{
+		ft_write_n(2, "pwd allocation error");
+		g_last_ret = 2;
+		return (1);
+	}
+	/*
 	if (!(pwd = get_env(envp, "PWD")))
 	{
 		ft_write_n(2, "pwd allocation error");
 		g_last_ret = 2;
 		return (1);
 	}
+	*/
 	g_last_ret = 0;
 	ft_write_n(1, pwd);
 	return (0);
