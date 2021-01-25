c9c946d4c8449d0cf84038646db44454f0c37d03
congcoding <congcoding@gmail.com>
2021-01-25 20:22:07 +0900

    cd fix cd에 인자 없는 경우 HOME으로 이동 error처리 함수화

--- srcs/builtin/Cd/cd.c
+++ srcs/builtin/Cd/cd.c
@@ -3,36 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: seolim <seolim@student.42.fr>              +#+  +:+       +#+        */
+/*   By: jbeen <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/13 13:46:31 by seolim            #+#    #+#             */
-/*   Updated: 2021/01/13 13:46:48 by seolim           ###   ########.fr       */
+/*   Updated: 2021/01/25 20:17:12 by jbeen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	cd(char *dir)
+static void	cd_error(char *dir)
+{
+	ft_write(2, "cd: ");
+	ft_write(2, dir);
+	ft_write(2, ": ");
+	ft_write_n(2, strerror(errno));
+	g_last_ret = 1;
+}
+
+int			cd(char *dir)
 {
 	char	pwd[260];
 
 	ft_strcpy(pwd, "PWD=");
+	if (!dir)
+		dir = get_env(g_envp, "HOME");
 	if (chdir(dir))
 	{
-		ft_write(2, "cd: ");
-		ft_write(2, dir);
-		ft_write(2, ": ");
-		ft_write_n(2, strerror(errno));
-		g_last_ret = 1;
+		cd_error(dir);
 		return (FALSE);
 	}
 	if (!getcwd(pwd + 4, 255))
 	{
-		ft_write(2, "cd: ");
-		ft_write(2, dir);
-		ft_write(2, ": ");
-		ft_write_n(2, strerror(errno));
-		g_last_ret = 1;
+		cd_error(dir);
 		return (FALSE);
 	}
 	set_env(g_envp, pwd);
